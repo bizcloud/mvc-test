2025c6bbc8703555f33b380f51c49e531b822388
Aleksandr Fefelov <slstudio@yandex.ru>
2022-05-24 02:16:10 +0000

    index controller test added, some dependencies added

---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+final class IndexControllerTest extends TestCase
+{
+
+    private $http;
+
+    public function setUp(): void
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => getenv('APP_URL')]);
+    }
+
+    public function tearDown(): void
+    {
+        $this->http = null;
+    }
+
+
+    /** @test */
+    public function RequestIndexPage_GetResponseStatus200(): void
+    {
+
+        $response = $this->http->get('/');
+        $this->assertEquals(200, $response->getStatusCode());
+
+    }
+
+}
