1f2691943cf0453ea2993bcc56c38d42ec1d2104
Aleksandr Fefelov <slstudio@yandex.ru>
2022-05-26 01:16:13 +0000

    PHP Docs for Router

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,12 +4,17 @@ namespace Bizcloud\MVCTest\Controller;
 
 use Bizcloud\MVCTest\AbstractController;
 
+/**
+ *
+ */
 class IndexController extends AbstractController
 {
+    /**
+     * @return void
+     */
     public function IndexAction()
     {
         $this->render('index');
-
     }
 
 }
--- src/Router.php
+++ src/Router.php
@@ -6,11 +6,23 @@ use Bizcloud\MVCTest\Exception\ActionNotFound;
 use Bizcloud\MVCTest\Exception\ControllerNotFound;
 
 
+/**
+ * Parse routes and execute Controller action
+ */
 class Router
 {
 
+    /**
+     * Index page Controller name
+     */
     const INDEX_NAME = 'index';
 
+    /**
+     * Parse routes and execute Controller action
+     * @return mixed
+     * @throws ActionNotFound
+     * @throws ControllerNotFound
+     */
     public function run()
     {
         session_start();
@@ -19,12 +31,9 @@ class Router
         $controllerName = $this->getControllerNameFromRoutes($routes);
         $action = $this->getActionFromRoutes($routes);
 
-
-
         if (!class_exists($controllerName)) {
             throw new ControllerNotFound();
         }
-
 
 
         /** @var AbstractController $controller */
@@ -34,20 +43,30 @@ class Router
 
     }
 
+    /**
+     * @return array
+     */
     private function parseRoutes(): array
     {
         $path = $_SERVER['REQUEST_URI'] ?? null;
         return $path ? explode('/', $path) : [];
     }
 
+    /**
+     * @param array $routes
+     * @return string
+     */
     private function getControllerNameFromRoutes(array $routes): string
     {
-
         $route = $routes[1] ? strtolower($routes[1]) : self::INDEX_NAME;
         //return 'Controller\\' . ucfirst($route) . 'Controller';
         return 'Bizcloud\\MVCTest\\Controller\\' . ucfirst($route) . 'Controller';
     }
 
+    /**
+     * @param array $routes
+     * @return string
+     */
     private function getActionFromRoutes(array $routes): string
     {
         return count($routes)>2 ? ($routes[2] ? strtolower($routes[2]) : self::INDEX_NAME):self::INDEX_NAME;
